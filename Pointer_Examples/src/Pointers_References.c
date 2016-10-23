004e14f35e51da4546b1f134dd186d8a2ba31b00
rduvalwa5 <rduvalwa5@hotmail.com>
2016-10-23 15:50:19 -0700

    pointer reference

--- Pointer_Examples/src/Pointers_References.c
+++ Pointer_Examples/src/Pointers_References.c
@@ -1,7 +1,7 @@
 /*
  * Pointers_References.c
  *
- *  Created on: Apr 11, 2015
+ *  Created on: oct 23, 2016
  *      Author: rduvalwa2
  */
 
@@ -15,7 +15,10 @@ int main()
      */
     int size = 5;
     int arr2[] = {1,2,3,4,5,'\0'};
-    printf("Size is %d Size of array is %d\n",size, sizeof(arr2));
+    int arr1[] = {1,2,3,4,5};
+    printf("Size is %d Size of array is %d\n",size, (int)(sizeof(arr2)/sizeof(arr2[0])));
+    printf("Size is %d Size of array is %d\n",size, (int)(sizeof(arr1)/sizeof(arr1[0])));
+    printf("Null is \n", arr2[size]);
     int i = 0;
 //    arr2[size] = '\0';
 //    for(i=0;i<sizeof(arr2);i++)
@@ -23,29 +26,28 @@ int main()
 //      arr2[i] = i; // Initializing each element seperately
 //    }
     printf("Size of array is %d\n",sizeof(arr2));
-    printf("array is %d\n",arr2);
-    /*
+    printf("array is %d\n",*arr2);
+    printf("array is %d\n",&arr2);
+
     int el = 0;
-    for(el=0; el < sizeof(arr2); el++)
+    for(el=0; el < size; el++)
     {
-    	printf("el is %d \n", el);
-    	printf("arr2 %d \n",arr2[el]);
-    	printf("Refer to Address &arr2 address is %i \n",&arr2[el]);
-       	printf("Point to value at address *arr2 value = %d \n",*arr2 + el);
+    	printf("arr2 element %d is  %d \n",el, arr1[el]);
+    	printf("Refer to Address &arr2[el] address is %i \n",&arr1[el]);
+       	printf("Pointer address is %d Point to value at address *arr1 value = %d \n",&arr1[el], *arr1 + el);
     }
 
-*/
-/*
+
     for(el=sizeof(arr2); el > -1; el--)
     {
     	printf("el is %d \n", el);
     	printf("arr2 %d \n",arr2[el]);
     	printf("Refer to Address &arr2 address is %i \n",&arr2[el]);
-       	printf("Point to value at address *arr2 value = %d \n",*arr2 + el);
+       	printf("X Point to value at address *arr2 value = %d \n",*arr2 + el);
     }
-*/
+
     /* do it in reverse */
-/*
+
     int reversed[size]; // still don't know why array size ends up 4 time declared size
     reversed[size]= '\0';
 
@@ -57,8 +59,7 @@ int main()
     	index++;
     }
     printf("reversed array is %s",&reversed);
-*/
-/*
+
     for(el=0;el < sizeof(reversed);el++)
     {
     	printf("reversed[el] is %d \n",reversed[el]);
@@ -71,7 +72,7 @@ int main()
     	printf("%s \n",&charArray[i]);
     }
 
- */
+
 
     return 0;
 }
