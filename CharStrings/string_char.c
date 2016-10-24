cfaff18b1bd29585dc391e37fb7dc8d2196e7357
rduvalwa5 <rduvalwa5@hotmail.com>
2016-10-23 22:42:39 -0700

    commit CharStrings

---
+++ CharStrings/string_char.c
@@ -0,0 +1,61 @@
+/*
+ * Exercise find longest string in an array of strings that contains all the
+ * characters in a char array
+ */
+
+#include <stdio.h>;
+#include <string.h>;
+
+#define MX 5
+#define LN 16
+
+char strg[MX][LN];
+
+int main(void)
+{
+/*
+Declaring a string.  This example is a char array, size is set in one example and defined by
+content it other example.
+*/
+	char chr[4] = "aces";
+	char word[] = "The word is OMmmm!";
+	
+	printf("chr is %s\n", chr);
+	printf("chr length id %d\n", strlen(chr));
+	printf("word is %s\n", word);
+	printf("word length id %d\n", strlen(word));
+	printf("sizeof chr is %d\n",sizeof(chr));
+	printf("sizeof first character of chr, \"%c\" is %d\n",chr[0], sizeof(chr[0]));
+
+	int c = 0;
+	/*
+	use in for loop
+	*/
+	for( c = 0; c < strlen(chr) ; c++)
+		{ printf("%c", chr[c]); 
+		}
+	printf("\n");
+	/*
+	Print in reverse using for loop
+	*/		
+	for( c = strlen(word); c > -1 ; c--)
+		{ printf("%c", word[c]); 
+		}
+	printf("\n");
+	
+//	strg[0][0] = "bases"; //,"aces","cases","seca","interfaces"};
+//	printf("%c\n",&strg[0]);
+//	str[0] = "bases";
+/*
+//	printf("%s\n",chr);
+//	printf("%d\n",sizeof(str));
+//	printf("%d\n",sizeof(str[0]));
+*/
+/*
+	for(int i=0;i<sizeof(str);i++){
+		printf("In for loop\n");
+		printf("%s \n",&str[i]);
+	}
+	*/
+	return(0);
+}
