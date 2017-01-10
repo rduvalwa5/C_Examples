0a72985575ad3382177654f5ecfdee218ea39da7
rduvalwa5 <rduvalwa5@hotmail.com>
2017-01-09 21:35:47 -0800

    update cpointer %p %x %u

--- Cpointers/cpointers.c
+++ Cpointers/cpointers.c
@@ -9,6 +9,14 @@
 
 int main(){
 
+	/*
+	 Functions belonging to the printf function family have the type specifiers "%p" and "%x".
+	 "x" and "X" serve to output a hexadecimal number. "x" stands for lower case letters (abcdef)
+	 while "X" for capital letters (ABCDEF). "p" serves to output a pointer. It may differ depending
+	 upon the compiler and platform.
+	 %d is a signed integer, while %u is an unsigned integer. Pointers (when treated as numbers) are
+	 usually non-negative. If you actually want to display a pointer, use the %p format specifier.
+	 */
 
 	char *c = "A"; // create and assign a pointer
 
@@ -20,14 +28,14 @@ int main(){
 	char * str = "My string!"; //create a string and assign a pointer
 	printf("the String %s\n", str);
 	for(int i = 0; i < strlen(str); i++){
-		printf("%c  %d\n",str[i],&str[i]);
+		printf("c is %c | d is %d | p is %p | x is  %x  | u is %u\n",str[i],&str[i],&str[i],&str[i],&str[i]);
 	}
 
 	int *ptr1, *ptr2;
 	ptr1 = str[1];
 	ptr2 = str[4];
 
-	printf("ptr1 %s value is %d at %d ptr2 value %d at %d \n", &ptr1, ptr1, &ptr1, ptr2,&ptr2);
+	printf("ptr1 %s value is %d at %d ptr2 value %d at %d or %p\n", &ptr1, ptr1, &ptr1, ptr2,&ptr2,&ptr2);
 
 }
 
